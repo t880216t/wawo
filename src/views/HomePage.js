9474471c4ab3e7b208505ea82af5bac2b0ecb1b5
t880216t <562746248@qq.com>
2017-09-05 15:51:18 +0800

    首页头部demo

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -4,7 +4,10 @@ import {
     StyleSheet,
     Text,
     View,
-    Dimensions
+    Dimensions,
+    StatusBar,
+    Image,
+    TextInput
 } from 'react-native';
 
 //antd搜索组件
@@ -29,20 +32,32 @@ export default class HomePage extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <StatusBar backgroundColor='#211510'></StatusBar>
                 <View style={styles.head}>
-                    <SearchBar
-                        value={this.state.value}
-                        placeholder="搜索"
-                        onSubmit={value => console.log(value, 'onSubmit')}
-                        onClear={value => console.log(value, 'onClear')}
-                        onFocus={() => console.log('onFocus')}
-                        onBlur={() => console.log('onBlur')}
-                        onCancel={() => console.log('onCancel')}
-                        showCancelButton
-                        onChange={value => {this.onChange(value)}}
-                    />
+                    <View style={styles.titleProfile}>
+                        <View style={{flex:1}}>
+                            <Image
+                                source={{uri:'http://oi2rzqa3e.bkt.clouddn.com/static/images/11-0.gif'}}
+                                style={styles.AdvImage}/>
+                        </View>
+                        <View style={styles.titleName}>
+                            <Text style={{color:'#f8b700', fontSize:15}}>未知目标</Text>
+                        </View>
+                        <View style={{flex:1}}></View>
+                    </View>
+                    <View style={styles.searchBox}>
+                        <Text>筛选</Text>
+                        <View>
+                            <TextInput style={styles.inputText}
+                                       underlineColorAndroid='transparent'
+                                       keyboardType='web-search'
+                                       placeholder='搜索' />
+                        </View>
+                    </View>
                 </View>
-                <Text>home</Text>
+                <View>
+
+                </View>
             </View>
         );
     }
@@ -55,8 +70,36 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
     },
     head:{
+        flex:1,
         width:Dimensions.get('window').width,
-        backgroundColor:'red',
-        alignItems:'center',
-    }
+    },
+    titleProfile:{
+        flexDirection:'row',
+        height:50,
+        backgroundColor:'#211510'
+    },
+    AdvImage:{
+        flex:1,
+        width:40,
+        height:40,
+        marginLeft:30,
+        marginTop:5,
+        marginBottom:5,
+        borderRadius:5
+    },
+    titleName:{
+        flex:3,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    inputText:{
+        fontSize:14,height:40, flex:1, borderColor: 'gray',borderWidth: 1
+    },
+    searchBox:{//搜索框
+        height:40,
+        flexDirection: 'row',   // 水平排布
+        borderRadius: 5,  // 设置圆角边
+        backgroundColor: 'gray',
+        alignItems: 'center',
+    },
 })
