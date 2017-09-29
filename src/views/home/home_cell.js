ac1342fac50510da64afb95d0f87a43bacd4663c
t880216t <562746248@qq.com>
2017-09-29 23:23:57 +0800

    出售详情页

--- src/views/home/home_cell.js
+++ src/views/home/home_cell.js
@@ -12,9 +12,28 @@ import {
 } from 'react-native';
 
 export default class HomeCell extends Component {
+    timeImage = (cellData) => {
+        prodTime = cellData.createTime
+        //formatTime = (new Date(prodTime)).toISOString().slice(0,10);
+        formatTime = (new Date(prodTime));
+        currentTime = (new Date());
+        var diffTime = ((currentTime - formatTime) / (24 * 3600 * 1000)).toFixed(1)
+        console.log('this difftime :',diffTime)
+        if(diffTime >= 15){
+            return <Image source={{uri:'https://www.battlenet.com.cn/wow/static/images/character/auction/time-verylong.png'}} style={{height:20,width: 20}}></Image>
+        }if(diffTime >= 10){
+            return <Image source={{uri:'https://www.battlenet.com.cn/wow/static/images/character/auction/time-long.png'}} style={{height:20,width: 20}}></Image>
+        }if(diffTime >= 5){
+            return <Image source={{uri:'https://www.battlenet.com.cn/wow/static/images/character/auction/time-medium.png'}} style={{height:20,width: 20}}></Image>
+        }else {
+            return <Image source={{uri:'https://www.battlenet.com.cn/wow/static/images/character/auction/time-short.png'}} style={{height:20,width: 20}}></Image>
+        }
+
+    }
 
     render() {
         let {cellData} = this.props;
+        var time = this.timeImage(cellData);
         return (
             <View>
                 <TouchableOpacity
@@ -31,7 +50,14 @@ export default class HomeCell extends Component {
                         </View>
                         <View style={{flex:7,justifyContent: 'center'}}>
                             <View style={{marginLeft: 10,flex:1,flexDirection: 'column'}}>
-                                <Text style={styles.cell_conntent}>{cellData.title}</Text>
+                                <View style={styles.cell_conntent}>
+                                    <View style={{flex:8}}>
+                                        <Text style={{fontSize: 15,color:'#c600ff'}}>{cellData.title}</Text>
+                                    </View>
+                                    <View style={{flex:1,margin: 5}}>
+                                        {time}
+                                    </View>
+                                </View>
                                 <View style={styles.cell_more}>
                                     <Text style={styles.cell_price}>¥ <Text >{cellData.price}</Text></Text>
                                     <TouchableOpacity style={{flex:3,alignItems:'flex-end',marginRight: 10}}
@@ -70,13 +96,13 @@ const styles = StyleSheet.create({
     },
     cell_conntent:{
         flex:2,
-        fontSize: 15,
-        color:'#c600ff',
-        marginTop:10
+        marginTop:10,
+        flexDirection:'row'
     },
     cell_price:{
         flex:2,
         color:'#c28a13',
+        marginTop:5
     },
     cell_more:{
         flex:2,
--- src/views/sale/sale_detail.js
+++ src/views/sale/sale_detail.js
@@ -15,9 +15,22 @@ export default class SaleDetail extends Component {
         tabBarVisible:false,
     };
 
+    showImage = (images) =>{
+        if (images){
+            return(
+                images.map((image)=>{
+                    <Image style={{width: 320, height: 320,marginTop: 5}} source={{uri:image}} key={image}></Image>
+                })
+            )
+        }else {
+            return (<View/>)
+        }
+    }
+
     render() {
         const {params} = this.props.navigation.state;
         const {item} = params.cellData;
+        let Images = this.showImage(item.imageUrls);
         return (
             <View>
                 <ScrollView>
@@ -25,10 +38,65 @@ export default class SaleDetail extends Component {
                         style={styles.backgroundImage}
                         source={{uri:'https://www.battlenet.com.cn/wow/static/images/profile/sidebar-bg.jpg'}}
                     >
-                        <View style={{flex:1,marginLeft:10,justifyContent: 'center',alignItems: 'center'}}>
-                            <Text style={{fontSize:15,color: '#c600ff',}}>
-                                {item.title}
-                            </Text>
+                        <View style={{flex:1,margin: 8,borderRadius: 8,backgroundColor: 'rgba(0,0,0,0.3)'}}>
+                            <View style={{margin: 10,flexDirection: 'row'}}>
+                                <View style={{flex:2}}>
+                                    <Image source={require('../../image/Empty.png')} style={{height: 90,width: 90,justifyContent: 'center',alignItems: 'center'}}>
+                                        <Image source={{uri:item.titleImage}} style={{height: 52,width: 52,borderRadius: 5,marginRight: 1,marginBottom: 2}}></Image>
+                                    </Image>
+                                </View>
+                                <View style={{flex:5,justifyContent:'center'}}>
+                                    <Text style={{color: 'white'}}>{item.title}</Text>
+                                </View>
+                            </View>
+
+                            <View style={{marginLeft:10,flexDirection: 'column',backgroundColor: 'rgba(0,0,0,0)'}}>
+                                <View style={{flexDirection: 'row'}}>
+                                    <View style={{marginLeft: 15,flex:2,justifyContent:'center'}}>
+                                        <Text style={{color:'#c09a67',fontSize: 15}}>出售价格:</Text>
+                                    </View>
+                                    <View style={{marginLeft: 15,borderRadius: 3,justifyContent:'center',flex:7,marginRight: 10}}>
+                                        <Text style={{backgroundColor: 'black',borderRadius:5,color: 'black',height:40,color: 'white'}}>
+                                            {item.price}
+                                        </Text>
+                                    </View>
+                                </View>
+                                <View style={{flexDirection: 'row',marginTop:10}}>
+                                    <View style={{marginLeft: 15,justifyContent:'center'}}>
+                                        <Text style={{color:'#c09a67',fontSize: 15,alignItems: 'flex-end'}}>        成色:</Text>
+                                    </View>
+                                    <View style={{marginLeft: 15,height:40,justifyContent:'center',padding: 5,backgroundColor: 'black',borderRadius: 3,flexDirection:'row'}}>
+                                        <Text
+                                            style={{backgroundColor: 'black',borderRadius: 3,justifyContent:'center'}}>
+                                            {item.chense}
+                                        </Text>
+                                    </View>
+                                </View>
+                                <View style={{flexDirection: 'row',marginTop:10}}>
+                                    <View style={{marginLeft: 15,justifyContent:'flex-start',flex:2}}>
+                                        <Text style={{color:'#c09a67',fontSize: 15}}>物品描述:</Text>
+                                    </View>
+                                    <View style={{borderRadius: 3,justifyContent:'flex-start',marginRight: 10,flex:6}}>
+                                        <Text style={{backgroundColor: 'black',borderRadius:5,minHeight:100,color: '#1EFF00'}}>
+                                            {item.description}
+                                        </Text>
+                                    </View>
+                                </View>
+                                <View style={{flex:1,margin: 10}}>
+                                    <Text style={{color:'#c09a67',fontSize: 15,marginTop: 10,marginLeft: 5}}>添加图片:</Text>
+
+                                    <View style={{marginTop: 10,alignItems: 'center', justifyContent: 'center',backgroundColor: 'black',}}>
+                                        {Images}
+                                    </View>
+                                </View>
+                            </View>
+                            <TouchableOpacity
+                                style={{justifyContent:'center',flex:1,alignItems: 'center',margin: 10}}
+                            >
+                                <Image source={require('../../image/_button.png')} style={{height: 50,justifyContent: 'center'}}>
+                                    <Text style={{color:'#ffae00',fontSize: 18,textAlign: 'center'}}>提交</Text>
+                                </Image>
+                            </TouchableOpacity>
                         </View>
                     </Image>
                 </ScrollView>
